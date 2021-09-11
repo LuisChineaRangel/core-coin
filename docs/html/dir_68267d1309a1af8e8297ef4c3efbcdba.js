361bea8d6487af7f77c9c7ec687cddf9c14d8ee7
Luis Marcelo Chinea Rangel <alu0101118116@ull.edu.es>
2021-09-12 00:47:12 +0100

    Updating

---
+++ docs/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,8 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "change.cpp", "d6/d6e/change_8cpp.html", "d6/d6e/change_8cpp" ],
+    [ "coin.cpp", "d6/d80/coin_8cpp.html", "d6/d80/coin_8cpp" ],
+    [ "exception.cpp", "de/d9e/exception_8cpp.html", null ],
+    [ "greedy.cpp", "df/d54/greedy_8cpp.html", "df/d54/greedy_8cpp" ],
+    [ "main.cpp", "df/d0a/main_8cpp.html", "df/d0a/main_8cpp" ]
+];
--- docs/html/files_dup.js
+++ docs/html/files_dup.js
@@ -1,4 +1,4 @@
 var files_dup =
 [
-    [ "Documents", "dir_f396504afdc959660871b003fa10164d.html", "dir_f396504afdc959660871b003fa10164d" ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
