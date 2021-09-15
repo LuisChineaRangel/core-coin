1a615e92bddd1491411fa6c7df080b321af76224
Luis Marcelo Chinea Rangel <alu0101118116@ull.edu.es>
2021-09-16 00:31:44 +0100

    Updating Documentation

--- docs/html/navtreeindex0.js
+++ docs/html/navtreeindex0.js
@@ -1,24 +1,5 @@
 var NAVTREEINDEX0 =
 {
-<<<<<<< HEAD
-"d6/d6e/change_8cpp.html":[1,0,0,0],
-"d6/d6e/change_8cpp.html#aa78a781363eedd9e57fb7d69de67d321":[1,0,0,0,0],
-"d6/d80/coin_8cpp.html":[1,0,0,1],
-"d6/d80/coin_8cpp.html#af6488b1e9830fd6effece8fd91889750":[1,0,0,1,0],
-"de/d9e/exception_8cpp.html":[1,0,0,2],
-"df/d0a/main_8cpp.html":[1,0,0,4],
-"df/d0a/main_8cpp.html#a0bed8474bd33a912769360766f6b10d4":[1,0,0,4,1],
-"df/d0a/main_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627":[1,0,0,4,2],
-"df/d0a/main_8cpp.html#a3ca8454445b0739f5894088d4203319b":[1,0,0,4,3],
-"df/d0a/main_8cpp.html#a518288848eb2471573b6581060452931":[1,0,0,4,0],
-"df/d54/greedy_8cpp.html":[1,0,0,3],
-"df/d54/greedy_8cpp.html#aa016cdf0c5d86271d57766565a19dc44":[1,0,0,3,0],
-"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[1,0,0],
-"files.html":[1,0],
-"globals.html":[1,1,0],
-"globals_func.html":[1,1,1],
-"globals_vars.html":[1,1,2],
-=======
 "annotated.html":[1,0],
 "classes.html":[1,1],
 "d0/d6f/class_negative_value.html":[1,0,4],
@@ -101,7 +82,6 @@ var NAVTREEINDEX0 =
 "globals_func.html":[2,1,1],
 "globals_vars.html":[2,1,2],
 "hierarchy.html":[1,2],
->>>>>>> Dev
 "index.html":[],
 "index.html":[0],
 "index.html#autotoc_md10":[0,1],
